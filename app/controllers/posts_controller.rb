45199e71518b725d3390735628c3a9f1c75e0e0a
shouxingzi <79133015+shouxingzi@users.noreply.github.com>
2021-03-14 16:08:52 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,22 @@
 class PostsController < ApplicationController
-  
   def index
     @posts = Post.all.order(id: "DESC")
   end
 
-
   def create
-    Post.create(content: params[:content])
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
   end
 
+  def checked
+    post = Post.find(params[:id])
+    if post.checked # if post.checked == true の略 もし既読であれば
+      post.update(checked: false) # 既読であれば既読を解除して更新
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id]) # 最後に更新したレコードを取得し直しitemへ代入
+    render json: { post: item } #JSON形式でpost:itemを出力
+  end
 end
